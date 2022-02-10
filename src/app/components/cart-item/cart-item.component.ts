86a5e4708ca49fa7f1c466575bee847b20a11dc4
baranyai.mark <baranyaimark17@gmail.com>
2022-02-10 21:59:14 +0100

    show editor with portal

--- src/app/components/cart-item/cart-item.component.ts
+++ src/app/components/cart-item/cart-item.component.ts
@@ -1,21 +1,66 @@
-import { Component, Input, OnInit } from '@angular/core';
+import {
+  CdkPortal,
+  CdkPortalOutlet,
+  DomPortalHost,
+  DomPortalOutlet,
+  PortalOutlet,
+} from '@angular/cdk/portal';
+import {
+  ApplicationRef,
+  Component,
+  ComponentFactoryResolver,
+  HostBinding,
+  Injector,
+  Input,
+  OnDestroy,
+  OnInit,
+  ViewChild,
+  ViewContainerRef,
+} from '@angular/core';
 
 @Component({
   selector: 'app-cart-item',
   templateUrl: './cart-item.component.html',
   styleUrls: ['./cart-item.component.scss'],
 })
-export class CartItemComponent implements OnInit {
+export class CartItemComponent implements OnInit, OnDestroy {
+  @ViewChild(CdkPortal) editorPortal!: CdkPortal;
+
   @Input() name!: string;
   description = '';
 
+  @HostBinding('class.editor-open')
   descriptionEditorIsOpen: boolean = false;
 
-  constructor() {}
+  private portalOutlet!: PortalOutlet;
 
-  ngOnInit(): void {}
+  constructor(
+    private readonly cfr: ComponentFactoryResolver,
+    private readonly appRef: ApplicationRef,
+    private readonly injector: Injector
+  ) {}
+
+  ngOnInit(): void {
+    this.portalOutlet = new DomPortalOutlet(
+      document.body,
+      this.cfr,
+      this.appRef,
+      this.injector
+    );
+  }
+
+  ngOnDestroy(): void {
+    this.portalOutlet.detach();
+    this.portalOutlet.dispose();
+  }
 
   toggleDescriptionEditor(): void {
     this.descriptionEditorIsOpen = !this.descriptionEditorIsOpen;
+
+    if (this.descriptionEditorIsOpen) {
+      this.portalOutlet.attach(this.editorPortal);
+    } else {
+      this.portalOutlet.detach();
+    }
   }
 }
